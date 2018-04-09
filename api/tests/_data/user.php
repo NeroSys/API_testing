86eedc9ec2d148811254cdbcc9ae6edda301d48f
roman <dda.nesterenko@gmail.com>
2018-04-09 16:26:36 +0300

    Initial commit

---
+++ api/tests/_data/user.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    [
+        'id' => 1,
+        'username' => 'admin',
+        'auth_key' => 'gjAV6M4B8g68lPDZ5E29yArspGFhgqzR',
+        // password_0
+        'password_hash' => '$2y$13$WEkUG926kA/2U2PkBgBdAu0VPhGIBBXqBulaIHpiaAs/SqsYzS6vm',
+        'password_reset_token' => null,
+        'created_at' => '1523001125',
+        'updated_at' => '1523001125',
+        'email' => 'admin@admin.ua',
+        'description' => 'Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів. "Риба" не тільки успішно пережила п\'ять століть, але й прижилася в електронному верстуванні, залишаючись по суті незмінною. Вона популяризувалась в 60-их роках минулого сторіччя завдяки виданню зразків шрифтів Letraset, які містили уривки з Lorem Ipsum, і вдруге - нещодавно завдяки програмам комп\'ютерного верстування на кшталт Aldus Pagemaker, які використовували різні версії Lorem Ipsum.',
+    ],
+    [
+        'id' => 3,
+        'username' => 'test',
+        'auth_key' => 'ELQGP5KO8Cg_wW-Bn3YvsgQw6xeOKDxQ',
+        'password_hash' => '$2y$13$dsM6Ri9xoqgjtTee9VVGTO42WDDlFfFID7XE/z/2Ba2Qc0wNouAVq',
+        'password_reset_token' => null,
+        'created_at' => '1523008550',
+        'updated_at' => '1523008550',
+        'email' => 'test@test.ua',
+        'description' => null,
+    ],
+];
